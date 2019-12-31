03b3df00c7046f656033e1a11994e82fd36e5be9
rohith2232 <adunoorirohithkumar@gmail.com>
2019-12-31 07:49:10 +0530

    website with css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FeaturesComponent } from './features/features.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'Pricing',component: PricingComponent},
+  {path:'Features',component: FeaturesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
